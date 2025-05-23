20c367e98bb06e57032f93ce1959e5a40db6b7e5
Giovanni <gi.olavarria@duocuc.cl>
2025-05-23 11:07:45 -0400

    inicio proyecto!

---
+++ src/test/java/com/example/ecomarketspa/EcoMarketSpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomarketspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketSpa1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
